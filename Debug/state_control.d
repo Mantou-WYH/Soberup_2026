476a23201cfa271fda250298d103d52ccb01ff23
Mantou <1259269714@qq.com>
2026-02-25 18:31:48 +0800

    Delete Debug directory
    delete useless file

--- Debug/state_control.d
+++
@@ -1,8 +0,0 @@
-state_control.o :	../code/state_control.c
-../code/state_control.c :
-state_control.o :	..\code\element_line.h
-..\code\element_line.h :
-state_control.o :	..\code\state_control.h
-..\code\state_control.h :
-state_control.o :	..\code\line.h
-..\code\line.h :
